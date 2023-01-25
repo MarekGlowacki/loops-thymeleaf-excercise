36009edcff4b3c2abbb17a8dd04dd0067b1dd64d
User <user@its.me>
2023-01-25 19:05:59 +0100

    excersice with creating data to table with thymeleaf loops

---
+++ src/main/java/online/javafun/loopsthymeleafexcercise/LoopsThymeleafExcerciseApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.loopsthymeleafexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopsThymeleafExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoopsThymeleafExcerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/javafun/loopsthymeleafexcercise/LoopsThymeleafExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.loopsthymeleafexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopsThymeleafExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
